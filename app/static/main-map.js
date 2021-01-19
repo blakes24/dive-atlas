05386a6ee7ff4b146f6b76e1b05e4937e9dd1421
blakes24 <blakes24@gmail.com>
2021-01-18 20:12:15 -0800

    Improve search functionality

--- app/static/main-map.js
+++ app/static/main-map.js
@@ -2,12 +2,13 @@ mapboxgl.accessToken = mapToken;
 
 // create new map
 let map = new mapboxgl.Map({
-	container : 'map',
-	style     : 'mapbox://styles/blakes24/ckjoq7xhd1bgc19lljdhgtpod'
+	container       : 'map',
+	style           : 'mapbox://styles/blakes24/ckjoq7xhd1bgc19lljdhgtpod',
+	doubleClickZoom : false
 });
 
 // Add zoom and rotation controls to the map.
-map.addControl(new mapboxgl.NavigationControl());
+map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
 map.setZoom(1);
 map.setCenter([ -170, 20 ]);
 
@@ -19,49 +20,71 @@ const dropPin = new mapboxgl.Marker({
 	color : '#8FC0A9'
 });
 
-//place marker on point where map is double-clicked
+let pinOnMap = false;
+
+//place marker on point where map is double-clicked and clear search field
 map.on('dblclick', (e) => {
 	dropPin.setLngLat(e.lngLat).addTo(map);
+	$('#search-text').val('');
+	pinOnMap = true;
 });
 
-//touch screen: place marker on point where map is touched
-map.on('touchstart', (e) => {
-	setTimeout(() => {
-		dropPin.setLngLat(e.lngLat).addTo(map);
-	}, 100);
+$('#search-text').on('keyup', () => {
+	dropPin.remove();
+	pinOnMap = false;
 });
 
-// gets location of dropPin and returns nearby dive sites
-$('#map-search').on('click', async function search() {
-	// check for pin
-	if (dropPin.getLngLat() === undefined) {
-		$('#results').html('<p>Add a pin to map.</p>');
-		return;
+//touch screen: place marker on point where map is touched and held
+let pressTimer;
+
+map
+	.on('touchend', (e) => {
+		clearTimeout(pressTimer);
+		// Clear timeout
+		return false;
+	})
+	.on('touchstart', (e) => {
+		// Set timeout
+		pressTimer = window.setTimeout(function() {
+			dropPin.setLngLat(e.lngLat).addTo(map);
+			$('#search-text').val('');
+			pinOnMap = true;
+		}, 500);
+		return false;
+	});
+
+$('#show-list').on('click', () => {
+	if ($('#show-list').text() === 'Hide List') {
+		$('#show-list').text('Show List');
+	} else {
+		$('#show-list').text('Hide List');
 	}
-	// get coords of dropped pin
-	const lng = dropPin.getLngLat().lng;
-	const lat = dropPin.getLngLat().lat;
-
-	//clear previous search results
-	$('#site-list').html('');
-	clearMarkers();
-
-	await getSites(lng, lat);
 });
 
-// gets search form data snd returns matching dive sites
+// use pin location or search form data to find matching dive sites
 $('#search-form').on('submit', async function textSearch(e) {
 	e.preventDefault();
-	// clear previous search results
-	$('#site-list').html('');
+	//clear previous search results
 	clearMarkers();
-	// get new search input
-	let str = $('#search-text').val();
+	$('#show-list').text('Searching...');
 
-	// check if input is blank
-	if (str.trim().length > 0) {
-		await getSearchResults(str);
+	// check for pin
+	if (pinOnMap === true) {
+		// get coords of dropped pin
+		const lng = dropPin.getLngLat().lng;
+		const lat = dropPin.getLngLat().lat;
+		// add search results
+		await getSites(lng, lat);
 	} else {
-		$('#search-text').val('Enter a search term');
+		// get search input
+		let str = $('#search-text').val();
+		// check if input is blank
+		if (str.trim().length > 0) {
+			// populate search results
+			await getSearchResults(str);
+		} else {
+			$('#show-list').text('Enter a search term or add a pin');
+			$('#results').html('<p>Enter a search term or add a pin to the map</p>');
+		}
 	}
 });
